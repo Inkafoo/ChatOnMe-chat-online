65069ce243865471061fc535d180cfb895a83128
Inkafoo <inkafoo@gmail.com>
2019-09-27 16:32:23 +0200

    designed welcome view

---
+++ app/src/main/java/com/example/chatonme/WelcomeActivity.kt
@@ -0,0 +1,17 @@
+package com.example.chatonme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+}
